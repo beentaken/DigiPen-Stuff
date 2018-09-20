8af54816651a5dfe90594084180da6851057029c
PowerUser00 <blakelysnorth@gmail.com>
2018-09-19 21:25:06 -0700

    Update

---
+++ RND/RND.c
@@ -0,0 +1,22 @@
+/* gcc -O -Wall -Wextra -ansi -pedantic -o hello HelloWorld.c */
+
+#include <time.h>
+#include <stdiolib.h>
+#include <stdio.h>
+
+int rnd main (void)
+{
+	int randnum = 0;
+	srand(time(NULL)
+	
+	rnd = rand() % ++6;
+	
+	printf("Your number is %i\n", rnd)
+	
+	rnd = rand() % ++6;
+	
+	printf("Your number is %i\n", rnd)
+	
+	
+	return 0;
+}
