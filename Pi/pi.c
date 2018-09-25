5c6711bc45f5eaa6a8017a268a768fc0a376aaec
Blakely North <blakely.north@dit1921us.DIGIPEN.EDU>
2018-09-24 17:43:18 -0700

    Made Pi and MC

---
+++ Pi/pi.c
@@ -0,0 +1,26 @@
+/********************
+FileName:
+Name:
+Date:
+Brief Description:
+
+note: -Werror treats all warnings as errors, use this when you want to make sure there are no warnings.
+gcc -Wall -Wextra -Werror -O -ansi -pedantic -o pi.exe main.c pi.c
+*********************/
+/*sqrt*/
+#include <math.h>
+/* Calculates PI using a quarter circle */
+double circle_pi(int rectangles)
+{
+
+	/*this is to get rid of the initial unused variable warning, change what is returned and use the variable! STUB*/
+	return rectangles;
+}
+/* Calculates PI using a series */
+double leibniz_pi(int iterations)
+{
+
+
+	/*this is to get rid of the initial unused variable warning, change what is returned and use the variable! STUB*/
+	return iterations;
+}
