19b678624d0aad574d5008b9af61ca88069bfc2c
Blakely North <blakely.north@dit1921us.DIGIPEN.EDU>
2018-09-28 16:50:15 -0700

    Did stuff gtg

--- Errors Abound/main .c
+++ Errors Abound/main .c
@@ -8,7 +8,6 @@ gcc -Wall -Wextra -O -ansi -pedantic -o tripleCube.exe tripleCube.c main.c
 
 /* Function declarations */
 void print_cubes(void);
-void print_pythagorean_triples(int low, int high);
 
 
 int main(void)
@@ -17,7 +16,7 @@ int main(void)
   print_cubes();
 
     /* Print all of the triples from 1 up to 100        */
-    /* but not including 100  [1, 100)  */                
+    /* but not including 100  [1, 100)                  
   print_pythagorean_triples(1, 100);
 
   return 0;
