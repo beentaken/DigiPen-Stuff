bd516f87d532d2ee3ff9a29ffe56d6b5da64f7e4
Blakely North <blakely.north@dit1921us.DIGIPEN.EDU>
2018-10-17 16:22:17 -0700

    made a mainfunction within "NewFile.c"

--- NewFile.c
+++ NewFile.c
@@ -5,10 +5,15 @@ Date Last Edited:
 Brief Description:
 gcc -Wall -Wextra -O -ansi -pedantic -o NewFile NewFile.c
 ****************************/
-
 /*printf, scanf*/
 #include <stdio.h>
 /*rand, srand*/
 #include <stdlib.h>
 /*time*/
 #include <time.h>
+
+int main(void)
+{
+
+  return 0;
+}
