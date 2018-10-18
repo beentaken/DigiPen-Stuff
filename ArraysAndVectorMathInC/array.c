bdc154c6e9809a32bdf5023feb14b27a608e9dcc
Blakely North <blakely.north@dit1921us.DIGIPEN.EDU>
2018-10-17 18:01:51 -0700

    A project about vector math.

---
+++ ArraysAndVectorMathInC/array.c
@@ -0,0 +1,32 @@
+/******************************************************************************
+Filename: array.c
+Author:
+Date:
+Brief Description:
+
+******************************************************************************/
+
+/**/
+void reverse_array(int a[], int size)
+{
+} 
+
+/**/
+void add_arrays(const int a[], const int b[], int c[], int size)
+{
+}
+
+/**/
+void scalar_multiply(int a[], int size, int multiplier)
+{
+}
+
+/**/
+int dot_product(const int a[], const int b[], int size)
+{
+}
+
+/**/
+void cross_product(const int a[], const int b[], int c[])
+{
+}
