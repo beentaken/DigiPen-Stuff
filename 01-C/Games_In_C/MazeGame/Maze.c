60ce665f71a92cf64ba29787449e7e7ec567097c
Blakely North <blakely.north@dit1921us.DIGIPEN.EDU>
2018-11-26 16:43:32 -0800

    Made Maze.c

---
+++ 01-C/Games_In_C/MazeGame/Maze.c
@@ -0,0 +1,19 @@
+/****************************
+Filename: Maze.c
+Name: Blakely North
+Date Last Edited:11-26-18
+Brief Description: Demonstrates some pointer stuff and multidimensional arrays. A maze Game.
+gcc -Wall -Wextra -O -ansi -pedantic -o Maze Maze.c
+****************************/
+/*printf, scanf*/
+#include <stdio.h>
+/*rand, srand*/
+#include <stdlib.h>
+/*time*/
+#include <time.h>
+
+int main(void)
+{
+  
+  return 0;
+}
