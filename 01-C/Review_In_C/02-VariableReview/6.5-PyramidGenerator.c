72372473dd912178770ac568bba1f675ec3028d0
PowerUser00 <blakelysnorth@gmail.com>
2018-12-13 19:16:33 -0800

    Worked on pyrgen.

--- 01-C/Review_In_C/02-VariableReview/6.5-PyramidGenerator.c
+++ 01-C/Review_In_C/02-VariableReview/6.5-PyramidGenerator.c
@@ -14,22 +14,22 @@ gcc -Wall -Wextra -O -ansi -pedantic -o a 6.5-PyramidGenerator.c
 
 int main(void)
 {
-    int x = 0, px = x, j, i, input, amntOfStars, amntOfSpaces;
-    printf("How tall should the pyramid be?");
+    int x = 1, j, i, input, amntOfSpaces;
+    printf("How tall should the pyramid be? ");
     scanf("%i", &input);
-    for (amntOfSpaces = input - 1, i = 0; i < input; i++, amntOfSpaces--)
-    {         
-        px = x, amntOfStars = (x * x - px * px);
-        for (j = 0; j < amntOfStars; j++)
+    for (i = 0, j = 0; i < input; i++, amntOfSpaces--)
+    {
+        for (j = 0, x += 2; j < x; j++)
         {
             putchar('*');
         }
+        /*
         for (j = 0; j < amntOfSpaces; j++)
         {
             putchar('\n');
         }
+        */
         putchar('\n');
     }
-    while (getchar() != '\n');
     return 0;
 }
