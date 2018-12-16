3781070bd7b0d5b193f5a9ef62c526744cdf8877
PowerUser00 <blakelysnorth@gmail.com>
2018-12-15 17:45:42 -0800

    Wrote stuff.

--- 01-C/Review_In_C/02-VariableReview/7-GCD.c
+++ 01-C/Review_In_C/02-VariableReview/7-GCD.c
@@ -1,9 +1,9 @@
 /****************************
-Filename: GCD.c
+Filename: 6.5-PyramidGenerator.c
 Name: Blakely North
 Date Last Edited:
 Brief Description:
-gcc -Wall -Wextra -O -ansi -pedantic -o a GCD.c
+gcc -Wall -Wextra -O -ansi -pedantic -o a 6.5-PyramidGenerator.c
 ****************************/
 /*printf, scanf*/
 #include <stdio.h>
@@ -14,6 +14,31 @@ gcc -Wall -Wextra -O -ansi -pedantic -o a GCD.c
 
 int main(void)
 {
+	char exit = 'n';
+	do
+	{
+		int starsToPut = -1, spacesToPut = -1, j, i, input, height;
+		printf("How tall should the pyramid be?\n(Enter the number 13579 to quit)\n");
+		scanf("%i", &input);
+		height = input;
+		spacesToPut = height - 1;
+		if (input != 13579)
+		{
+			for (starsToPut = 1, i = 0, j = 0; i < input; i++, height--)
+			{
+				for (j = 0; j < spacesToPut; j++)
+					putchar(' ');
 
-    return 0;
+				for (j = 0; j < starsToPut; j++)
+					putchar('*');
+
+				starsToPut += 2;
+				spacesToPut--;
+				putchar('\n');
+			}
+		}
+		if (input == 13579)
+			exit = 'y';
+	} while (exit != 'y');
+	return 0;
 }
