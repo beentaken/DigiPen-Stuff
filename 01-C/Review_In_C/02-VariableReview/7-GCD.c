81020c0861cde9975989fe58d86523d75288c023
PowerUser00 <blakelysnorth@gmail.com>
2018-12-15 20:48:21 -0800

    Made some quick adjustments to GCD.c

--- 01-C/Review_In_C/02-VariableReview/7-GCD.c
+++ 01-C/Review_In_C/02-VariableReview/7-GCD.c
@@ -25,7 +25,7 @@ Divide M by N (M/N) and assign the remainder to R.
 
 
 3-----------
-If R is not 0, then assign the value of N to M, assign the value of R to N, and return to 
+If R is not 0, then assign the value of N to M, assign the value of R to N, and return to step 1.
 
 If R = 0, then the GCD is N and the algorithm terminates.
 
@@ -33,29 +33,22 @@ If R = 0, then the GCD is N and the algorithm terminates.
 
 int main(void)
 {
-	char exit = 'n';
+	int M, N, R, i;
+	printf("Enter two numbers with a space between them.\n");
+	scanf("%i %i", &M, &N);
 	do
 	{
-		int M, N, R;
-		printf("Enter two numbers with a space between them.\n");
-		scanf("%i %i", &M, &N);
-		do
-		{
-			/* Assign the larger number to M, and the smaller number to N. */
-			if (M < N)
-				R = M, M = N, N = R, R = 0;
-			/* Divide M by N (M/N) and assign the remainder to R. */
-			R = M % N;
-			/*If R is not 0, then assign the value of N to M, assign the value of R to N, and return to  step 2*/
-			if (R != 0)
-				M = N, N = R;
-			/* If R = 0, then the GCD is N and the algorithm terminates.*/
-			else
-				printf("The GCD is %i", N);
-		} while (R != 0);
-		printf("Do you want to exit? [y/N]");
-		scanf("%c", &exit);
-
-	} while (exit == 'n' || exit == 'N');
+		/* Assign the larger number to M, and the smaller number to N. */
+		if (M < N)
+			i = M, M = N, N = i, i = 0;
+		/* Divide M by N (M/N) and assign the remainder to R. */
+		R = M % N;
+		/*If R is not 0, then assign the value of N to M, assign the value of R to N, and return to  step 2*/
+		if (R != 0)
+			M = N, N = R;
+		/* If R = 0, then the GCD is N and the algorithm terminates.*/
+		else
+			printf("The GCD is %i", N);
+	} while (R != 0);
 	return 0;
 }
