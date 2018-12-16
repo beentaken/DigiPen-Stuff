102a0051cf3cdddff85946a00b7e4664c0399936
PowerUser00 <blakelysnorth@gmail.com>
2018-12-15 20:32:00 -0800

    Almost finished GCD.c

--- 01-C/Review_In_C/02-VariableReview/7-GCD.c
+++ 01-C/Review_In_C/02-VariableReview/7-GCD.c
@@ -1,9 +1,9 @@
 /****************************
-Filename: 6.5-PyramidGenerator.c
+Filename: GCD.c
 Name: Blakely North
 Date Last Edited:
 Brief Description:
-gcc -Wall -Wextra -O -ansi -pedantic -o a 6.5-PyramidGenerator.c
+gcc -Wall -Wextra -O -ansi -pedantic -o a GCD.c
 ****************************/
 /*printf, scanf*/
 #include <stdio.h>
@@ -12,33 +12,50 @@ gcc -Wall -Wextra -O -ansi -pedantic -o a 6.5-PyramidGenerator.c
 /*time*/
 #include <time.h>
 
+/******************************
+Actions to be Performed
+
+
+1----------
+Assign the larger number to M, and the smaller number to N.
+
+
+2-----------
+Divide M by N (M/N) and assign the remainder to R.
+
+
+3-----------
+If R is not 0, then assign the value of N to M, assign the value of R to N, and return to 
+
+If R = 0, then the GCD is N and the algorithm terminates.
+
+*****************************************/
+
 int main(void)
 {
 	char exit = 'n';
 	do
 	{
-		int starsToPut = -1, spacesToPut = -1, j, i, input, height;
-		printf("How tall should the pyramid be?\n(Enter the number 13579 to quit)\n");
-		scanf("%i", &input);
-		height = input;
-		spacesToPut = height - 1;
-		if (input != 13579)
+		int M, N, R;
+		printf("Enter two numbers with a space between them.\n");
+		scanf("%i %i", &M, &N);
+		do
 		{
-			for (starsToPut = 1, i = 0, j = 0; i < input; i++, height--)
-			{
-				for (j = 0; j < spacesToPut; j++)
-					putchar(' ');
+			/* Assign the larger number to M, and the smaller number to N. */
+			if (M < N)
+				R = M, M = N, N = R, R = 0;
+			/* Divide M by N (M/N) and assign the remainder to R. */
+			R = M % N;
+			/*If R is not 0, then assign the value of N to M, assign the value of R to N, and return to  step 2*/
+			if (R != 0)
+				M = N, N = R;
+			/* If R = 0, then the GCD is N and the algorithm terminates.*/
+			else
+				printf("The GCD is %i", N);
+		} while (R != 0);
+		printf("Do you want to exit? [y/N]");
+		scanf("%c", &exit);
 
-				for (j = 0; j < starsToPut; j++)
-					putchar('*');
-
-				starsToPut += 2;
-				spacesToPut--;
-				putchar('\n');
-			}
-		}
-		if (input == 13579)
-			exit = 'y';
-	} while (exit != 'y');
+	} while (exit == 'n' || exit == 'N');
 	return 0;
 }
