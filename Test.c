e0d21623241d95b9e8e23cfb41778f409b841eed
PowerUser00 <blakelysnorth@gmail.com>
2018-12-16 22:02:06 -0800

    Moved and cleared test.c

---
+++ Test.c
@@ -0,0 +1,19 @@
+/****************************
+Filename: Test.c
+Name: Blakely North
+Date Last Edited:
+Brief Description:
+gcc -Wall -Wextra -O -ansi -pedantic -o Test Test.c
+****************************/
+/*printf, scanf*/
+#include <stdio.h>
+/*rand, srand*/
+#include <stdlib.h>
+/*time*/
+#include <time.h>
+
+int main(void)
+{
+  
+  return 0;
+}
