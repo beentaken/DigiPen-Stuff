e2dda87e48a0d522c08fd54876ab8724e2ef25d4
PowerUser00 <blakelysnorth@gmail.com>
2019-01-16 18:00:27 -0800

    More AI.

--- 01-C/Games_In_C/TicTacToeAI/TicTacToeAI.c
+++ 01-C/Games_In_C/TicTacToeAI/TicTacToeAI.c
@@ -1,209 +1,284 @@
-/****************************
-Filename: TicTacToeAI.c
-Name: Blakely North
-Date Last Edited: 1-15-2019
-Brief Description: TicTacToe with AI for the second AP Project practice.
-gcc -Wall -Wextra -O -ansi -pedantic -o TicTacToeAI TicTacToeAI.c
-****************************/
-/*printf, scanf*/
 #include <stdio.h>
-/*rand, srand*/
+#include <conio.h>
 #include <stdlib.h>
-/*time*/
-#include <time.h>
+#include <windows.h>
 
-#define TRUE 1
-#define FALSE 0
-/* Max length of the player's name*/
-#define NAMEMAXLENGTH 10
+int board[10] = {2, 2, 2, 2, 2, 2, 2, 2, 2, 2};
+int turn = 1, flag = 0;
+int player, comp;
 
-/*function will need to look at the board and determine if there is a winner or if it is a tie*/
-int CheckGrid(int board[][3]);
-/*print out the board for the players*/
-void printBoard(int board[][3]);
-/*what the ai does on its turn.*/
-int AITurn(int board[][3] int Pl_Input);
-
-int main(void)
+void menu();
+void go(int n);
+void start_game();
+void check_draw();
+void draw_board();
+void player_first();
+void put_X_O(char ch, int pos);
+COORD coord = {0, 0}; // this is global variable
+					  //center of axis is set to the top left cornor of the screen
+void gotoxy(int x, int y)
 {
-	/* Keeps track of who's turn it is. 0 means it's not the player's turn (AI's turn). 1 Means it is the player's turn (not the AI's turn).*/
-	int playerTurn = 1;
-	/* The player/AI Pl_Input */
-	int Pl_Input;
-	/* The game board. -1 is not occupied, 0 is O, 1 is X */
-	int board[3][3] = {{-1, -1, -1}, {-1, -1, -1}, {-1, -1, -1}};
-	/* Wether somone has won yet. (-1 = no win yet, 0 = AI won, 1 = Player won, 2 = Tie) */
-	int gamestate = -1;
-	char playerName[NAMEMAXLENGTH] = {'P', 'l', 'a', 'y', 'e', 'r'};
-	printf("Welcome to TicTacToe AI edition.\nPlease enter a player name with less than %i characters: ", NAMEMAXLENGTH);
-	scanf("%s", playerName);
-	putchar('\n');
-	printBoard(board);
-	/* */
-	while (gamestate == -1)
+	coord.X = x;
+	coord.Y = y;
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+}
+
+int main()
+{
+	system("cls");
+	menu();
+	getch();
+	return (0);
+}
+
+void menu()
+{
+	int choice;
+	system("cls");
+	printf("\n--------MENU--------");
+	printf("\n1 : Play with X");
+	printf("\n2 : Play with O");
+	printf("\n3 : Exit");
+	printf("\nEnter your choice:>");
+	scanf("%d", &choice);
+	turn = 1;
+	switch (choice)
 	{
-		if (playerTurn == 1)
-		{
-			printf("Your turn, %s\n", playerName);
-			scanf("%i", &Pl_Input);
-			if (Pl_Input > 0 && Pl_Input <= 9 && board[Pl_Input / 3][Pl_Input % 3] == -1)
-			{
-				/*legal Pl_Input found*/
-				board[Pl_Input / 3][Pl_Input % 3] = playerTurn;
-				break;
-			}
-			printf("Invalid Pl_Input\n");
-			while (getchar() != '\n');
-		}
-		else
-			board[Pl_Input / 3][Pl_Input % 3] = AITurn(board[3][3]);
-		playerTurn = !playerTurn;
-		gamestate = CheckGrid(board);
-	}
-	printBoard(board);
-	/* Who's the winner? */
-	switch (gamestate)
-	{
-	case 0:
-		printf("AI Wins.\n");
-		break;
 	case 1:
-		printf("%s Wins!\n", playerName);
+		player = 1;
+		comp = 0;
+		player_first();
 		break;
 	case 2:
-		printf("Tie!\n");
+		player = 0;
+		comp = 1;
+		start_game();
 		break;
+	case 3:
+		exit(1);
+	default:
+		menu();
+	}
+}
+
+int make2()
+{
+	if (board[5] == 2)
+		return 5;
+	if (board[2] == 2)
+		return 2;
+	if (board[4] == 2)
+		return 4;
+	if (board[6] == 2)
+		return 6;
+	if (board[8] == 2)
+		return 8;
+	return 0;
+}
+
+int make4()
+{
+	if (board[1] == 2)
+		return 1;
+	if (board[3] == 2)
+		return 3;
+	if (board[7] == 2)
+		return 7;
+	if (board[9] == 2)
+		return 9;
+	return 0;
+}
+
+int posswin(int p)
+{
+	// p==1 then X   p==0  then  O
+	int i;
+	int check_val, pos;
+
+	if (p == 1)
+		check_val = 18;
+	else
+		check_val = 50;
+
+	i = 1;
+	while (i <= 9) //row check
+	{
+		if (board[i] * board[i + 1] * board[i + 2] == check_val)
+		{
+			if (board[i] == 2)
+				return i;
+			if (board[i + 1] == 2)
+				return i + 1;
+			if (board[i + 2] == 2)
+				return i + 2;
+		}
+		i += 3;
+	}
+
+	i = 1;
+	while (i <= 3) //column check
+	{
+		if (board[i] * board[i + 3] * board[i + 6] == check_val)
+		{
+			if (board[i] == 2)
+				return i;
+			if (board[i + 3] == 2)
+				return i + 3;
+			if (board[i + 6] == 2)
+				return i + 6;
+		}
+		i++;
+	}
+
+	if (board[1] * board[5] * board[9] == check_val)
+	{
+		if (board[1] == 2)
+			return 1;
+		if (board[5] == 2)
+			return 5;
+		if (board[9] == 2)
+			return 9;
+	}
+
+	if (board[3] * board[5] * board[7] == check_val)
+	{
+		if (board[3] == 2)
+			return 3;
+		if (board[5] == 2)
+			return 5;
+		if (board[7] == 2)
+			return 7;
 	}
 	return 0;
 }
 
-/*function will need to look at the board and determine if there is a winner or if it is a tie*/
-int CheckGrid(int board[3][3])
+void go(int n)
 {
-	int i, j;
-
-	/*horizontal loop through all on one row*/
-	for (i = 0; i < 3; i++)
-		if (board[i][0] != -1 && board[i][0] == board[i][1] && board[i][1] == board[i][2])
-			return board[i][0];
-
-	/*verticle loop through all on one column*/
-	for (j = 0; j < 3; j++)
-		if (board[0][j] != -1 && board[0][j] == board[1][j] && board[1][j] == board[2][j])
-			return board[0][j];
-	/*diagonals check all at once*/
-
-	if (board[0][0] != -1 && board[0][0] == board[1][1] && board[1][1] == board[2][2])
-		return board[1][1];
-
-	/*other diagonal*/
-	if (board[0][2] != -1 && board[0][2] == board[1][1] && board[1][1] == board[2][0])
-		return board[1][1];
-
-	/*make sure not tie*/
-	for (i = 0; i < 3; i++)
-		for (j = 0; j < 3; j++)
-			/* -1 means the game isn't done */
-			if (board[i][j] == -1)
-				return -1;
-
-	/*it is a tie if it gets here*/
-	return 2;
+	if (turn % 2)
+		board[n] = 3;
+	else
+		board[n] = 5;
+	turn++;
 }
 
-/*print out the board for the players*/
-void printBoard(int board[][3])
+void player_first()
 {
-	int i, j;
-	for (i = 0; i < 3; i++)
+	int pos;
+
+	check_draw();
+	draw_board();
+	gotoxy(30, 18);
+	printf("Your Turn :> ");
+	scanf("%d", &pos);
+
+	if (board[pos] != 2)
+		player_first();
+
+	if (pos == posswin(player))
 	{
-		printf("|");
-		for (j = 0; j < 3; j++)
-		{
-			switch (board[i][j])
-			{
-			case -1:
-				printf(" %i |", i * 3 + j);
-				break;
-			case 0:
-				printf(" O |");
-				break;
-			case 1:
-				printf(" X |");
-				break;
-			}
-		}
-		printf("\n\n");
+		go(pos);
+		draw_board();
+		gotoxy(30, 20);
+		//textcolor(128+RED);
+		printf("Player Wins");
+		getch();
+		exit(0);
 	}
-	return 0;
+
+	go(pos);
+	draw_board();
+	start_game();
 }
 
-/*what the ai does on its turn.*/
-
-void AImove(char tempboard[3][3], int *rows, int *columns)
+void start_game()
 {
-	int points = -1, temppoints, i, i2;
-
-	for (i = 0; i < 3; i++)
+	// p==1 then X   p==0  then  O
+	if (posswin(comp))
 	{
-		for (i2 = 0; i2 < 3; i2++)
-		{
-			if (tempboard[i][i2] == '\0')
-			{
-				tempboard[i][i2] = 'O';
-				temppoints = MinMax(tempboard, 1);
-				if (temppoints > points)
-				{
-					points = temppoints;
-					*rows = i;
-					*columns = i2;
-				}
-				tempboard[i][i2] = '\0';
-			}
-		}
+		go(posswin(comp));
+		flag = 1;
+	}
+	else if (posswin(player))
+		go(posswin(player));
+	else if (make2())
+		go(make2());
+	else
+		go(make4());
+	draw_board();
+
+	if (flag)
+	{
+		gotoxy(30, 20);
+		//textcolor(128+RED);
+		printf("Computer wins");
+		getch();
+	}
+	else
+		player_first();
+}
+
+void check_draw()
+{
+	if (turn > 9)
+	{
+		gotoxy(30, 20);
+		//textcolor(128+RED);
+		printf("Game Draw");
+		getch();
+		exit(0);
 	}
 }
 
-int MinMax(char tempboard[3][3], int player)
+void draw_board()
 {
-	int winner, points, temppoints, i, i2;
-	winner = didwin(tempboard);
-	if (winner == 'X')
-		return (0);
-	else if (winner == 'O')
-		return (999);
+	int j;
+
+	for (j = 9; j < 17; j++)
+	{
+		gotoxy(35, j);
+		printf("|       |");
+	}
+	gotoxy(28, 11);
+	printf("-----------------------");
+	gotoxy(28, 14);
+	printf("-----------------------");
+
+	for (j = 1; j < 10; j++)
+	{
+		if (board[j] == 3)
+			put_X_O('X', j);
+		else if (board[j] == 5)
+			put_X_O('O', j);
+	}
+}
+
+void put_X_O(char ch, int pos)
+{
+	int m;
+	int x = 31, y = 10;
+
+	m = pos;
+
+	if (m > 3)
+	{
+		while (m > 3)
+		{
+			y += 3;
+			m -= 3;
+		}
+	}
+	if (pos % 3 == 0)	
+		x += 16;
 	else
 	{
-		for (i = 0; i < 3; i++)
+		pos %= 3;
+		pos--;
+		while (pos)
 		{
-			for (i2 = 0; i2 < 3; i2++)
-			{
-				if (tempboard[i][i2] = '\0')
-				{
-					if (player == 1)
-					{
-						tempboard[i][i2] = 'X';
-					}
-					else
-					{
-						tempboard[i][i2] = 'O';
-					}
-					if (player == 1)
-					{
-						temppoints = MinMax(tempboard, 2);
-					}
-					else
-					{
-						temppoints = MinMax(tempboard, 1);
-					}
-					if (temppoints > points)
-					{
-						points = temppoints;
-					}
-					tempboard[i][i2] = '\0';
-				}
-			}
+			x += 8;
+			pos--;
 		}
 	}
-	return (points);
+	gotoxy(x, y);
+	printf("%c", ch);
 }
