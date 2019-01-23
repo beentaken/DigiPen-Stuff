a2548803382df80cadafb8f32f5fd2d41107609b
PowerUser00 <blakelysnorth@gmail.com>
2019-01-23 15:41:14 -0800

    Update NewFile.c

--- NewFile.c
+++ NewFile.c
@@ -1,10 +1,10 @@
-/****************************
-Filename: NewFile.c
-Name: Blakely North
-Date Last Edited:
-Brief Description:
-gcc -Wall -Wextra -O -ansi -pedantic -o NewFile NewFile.c
-****************************/
+/******************************
+ * Filename: NewFile.c
+ * Name: Blakely North
+ * Date Last Edited: 1-22-2019
+ * Brief Description: Program shows printing out a string with a pointer
+ * gcc -Wall -Wextra -O -ansi -pedantic -o NewFile NewFile.c
+ ******************************/
 /*printf, scanf*/
 #include <stdio.h>
 /*rand, srand*/
