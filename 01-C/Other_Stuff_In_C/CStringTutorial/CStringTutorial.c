ff870772385088781dadec078360e65d5730fb07
PowerUser00 <blakelysnorth@gmail.com>
2019-01-23 17:38:44 -0800

    Worked on CStringTutorial.c. Not finished yet

--- 01-C/Other_Stuff_In_C/CStringTutorial/CStringTutorial.c
+++ 01-C/Other_Stuff_In_C/CStringTutorial/CStringTutorial.c
@@ -8,11 +8,11 @@
 #include <stdio.h>
 
 /* this function takes a string and counts the chars until a null char.*/
-int strlength(const char* string);
+int strlength(const char *string);
 /* this function accepts 2 strs as paramaters, one as a src and will overwrite the src to the destination, ensuring that the destination has space. returns a pointer to the new str. */
-char *strcpy(char *dest, const char *src);
+char *strcpy(const char *src, char *dest);
 /* this one ensures that dest has enough space to fit src.*/
-char* strcat(char* dest, const char src);
+char *strcat(const char *src, char *dest);
 int main(void)
 {
   char str1[256] = "Hello, my name is Inigo Montoya.";
@@ -33,23 +33,35 @@ int main(void)
 int strlength(const char *str)
 {
   int length;
-  char* s;
-  for(*s = str, length = 0; *s != '\0'; ++s, ++length)
-    *s = str;
+  for (length = 0; str[length] != '\0'; ++length)
+    printf("lol\n");
   return length;
 }
 /* this function accepts 2 strs as paramaters, one as a src and will overwrite the src to the destination, ensuring that the destination has space. returns a pointer to the new str. */
-char *strcpy(const char *src, char *dest)
+char* strcpy(const char *src, char *dest)
 {
-  char* s = src, d = dest;
-  while(*s != '\0')
+  char *s = src, *d = dest;
+  while (*s != '\0')
   {
-    f
+    *d = *s;
+    ++d;
+    ++s;
   }
+  *d = '\0';
   return dest;
 }
 /* this one ensures that dest has enough space to fit src.*/
-char *strcat(const char *src, char *dest)
+char* strcat(const char *src, char *dest)
 {
+  char *d = dest;
+  while (*d != '\0')
+    d++;
+  while (*src != '\0')
+  {
+    *d = *src;
+    d++;
+    src++;
+  }
+  *d = '\n';
   return dest;
 }
