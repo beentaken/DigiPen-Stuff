0bfb1efa966941eb5dfcc56b0e2b909605227431
PowerUser00 <blakelysnorth@gmail.com>
2019-01-25 18:03:43 -0800

    Update CStringTutorial.c

--- 01-C/Other_Stuff_In_C/CStringTutorial/CStringTutorial.c
+++ 01-C/Other_Stuff_In_C/CStringTutorial/CStringTutorial.c
@@ -3,7 +3,7 @@
  * Name: Blakely North
  * Date Last Edited:
  * Brief Description:
- * gcc -Wall -Wextra -O -ansi -pedantic -o CStringTutorial CStringTutorial.c
+   gcc -Wall -Wextra -O -ansi -pedantic -o CStringTutorial CStringTutorial.c
  ******************************/
 #include <stdio.h>
 
@@ -15,15 +15,15 @@ char *strcpy(const char *src, char *dest);
 char *strcat(const char *src, char *dest);
 int main(void)
 {
-  char str1[256] = "Hello, my name is Inigo Montoya.";
+  char str1[256] = "Hello, my name is Inigo Montoya. ";
   char str2[256] = "You killed my father, prepare to die!";
   puts(str1), puts(str2);
-  printf("Length of str1 is: %i", strlength(str1));
-  printf("Length of str2 is: %i", strlength(str2));
+  printf("\nLength of str1 is: %i\n", strlength(str1));
+  printf("\nLength of str2 is: %i\n", strlength(str2));
 
-  puts("Concatenating str's");
-  puts(strcat(str1, str2));
-  puts("Coppying str's");
+  puts("Concatenating strings");
+  puts(strcat(str2, str1));
+  puts("Coppying strings");
   puts(strcpy(str1, str2));
 
   return 0;
@@ -34,13 +34,14 @@ int strlength(const char *str)
 {
   int length;
   for (length = 0; str[length] != '\0'; ++length)
-    printf("lol\n");
+    putchar(str[length]);
   return length;
 }
 /* this function accepts 2 strs as paramaters, one as a src and will overwrite the src to the destination, ensuring that the destination has space. returns a pointer to the new str. */
-char* strcpy(const char *src, char *dest)
+char *strcpy(const char *src, char *dest)
 {
-  char *s = src, *d = dest;
+  const char *s = src;
+  char *d = dest;
   while (*s != '\0')
   {
     *d = *s;
@@ -51,7 +52,7 @@ char* strcpy(const char *src, char *dest)
   return dest;
 }
 /* this one ensures that dest has enough space to fit src.*/
-char* strcat(const char *src, char *dest)
+char *strcat(const char *src, char *dest)
 {
   char *d = dest;
   while (*d != '\0')
