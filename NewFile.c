53869bf3fc8f54cf533e829b9ae90c0c36b328c8
PowerUser00 <blakelysnorth@gmail.com>
2019-01-29 22:49:49 -0800

    Update NewFile.c

--- NewFile.c
+++ NewFile.c
@@ -2,7 +2,7 @@
  * Filename: NewFile.c
  * Name: Blakely North
  * Date Last Edited: 1-22-2019
- * Brief Description: Program shows printing out a string with a pointer
+ * Brief Description: A sample and standard new file
  * gcc -Wall -Wextra -O -ansi -pedantic -o NewFile NewFile.c
  ******************************/
 /*printf, scanf*/
