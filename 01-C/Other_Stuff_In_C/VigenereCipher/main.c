f658da769af208af9f14839e92afa80ef3ae30a3
PowerUser00 <blakelysnorth@gmail.com>
2019-01-29 22:58:32 -0800

    Edited the encryption loop to bee closer to working order.
    Enough with the boaring "Update <file>.c" commits.

--- 01-C/Other_Stuff_In_C/VigenereCipher/main.c
+++ 01-C/Other_Stuff_In_C/VigenereCipher/main.c
@@ -1,11 +1,11 @@
-/****************
-main.c
-By:
-Date: 
-Brief: The test file to use with vigenereCipher.c.  You will need to set it up to take input from the console for both the string to encrypt and the key to encrypt with, please use the provided arrays!  After encrypting also test the decrypting! 
-gcc -O -Wall -Wextra -ansi -pedantic -o vcipher vigenereCipher.c main.c
-****************/
-#include <stdio.h>
+/******************************
+ * Filename: NewFile.c
+ * Name: Blakely North
+ * Date Last Edited: 1-22-2019
+ * Brief: The test file to use with vigenereCipher.c.  You will need to set it up to take input from the console for both the string to encrypt and the key to encrypt with, please use the provided arrays!  After encrypting also test the decrypting! 
+ * Brief Description: Program shows printing out a string with a pointer
+ * gcc -Wall -Wextra -O -ansi -pedantic -o NewFile NewFile.c
+ ******************************/
 #include "vigenereCipher.h"
 
 int main(void)
@@ -17,7 +17,7 @@ int main(void)
 	/*ask for the string to encrypt and store it in string*/
 	printf("What phrase do you want to encrypt?\n>");
 	scanf(" %s", string);
-	
+
 	/*ask for the key to encrypt with and store it in key*/
 	printf("What encryption key do you want to use?\n>");
 	scanf(" %s", key);
@@ -30,10 +30,53 @@ int main(void)
 	*k = string;
 	for (i = 0; *k != '\0'; ++stringlen, ++k);
 	/*encrypt string with the key, make sure set to encode!*/
-	for (j = 0, i = 0; i < passes; i++, (int)string[i] += (key[i % 26]));
+	for (i = 0; i < passes; i++, string[i] = ((string[i] + key[i % keylen]) % 26) + 'A');
 	/*print out the encoded string*/
 	printf("The encrypted phrase is:\n%s", string);
 	/*now reverse it back to normal by setting it to decode and print it out again*/
 
+/******* Not my work below - Just a sample to work off incase I need help that I found on the Internet *******/
+
+/*#include<stdio.h>
+ *#include<string.h>
+ *
+ *int main(){
+ *    char msg[] = "THECRAZYPROGRAMMER";
+ *    char key[] = "HELLO";
+ *    int msgLen = strlen(msg), keyLen = strlen(key), i, j;
+ *
+ *    char newKey[msgLen], encryptedMsg[msgLen], decryptedMsg[msgLen];
+ *
+ *    //generating new key
+ *    for(i = 0, j = 0; i < msgLen; ++i, ++j){
+ *        if(j == keyLen)
+ *            j = 0;
+ *
+ *        newKey[i] = key[j];
+ *    }
+ *
+ *    newKey[i] = '\0';
+ *
+ *    //encryption
+ *    for(i = 0; i < msgLen; ++i)
+ *        encryptedMsg[i] = ((msg[i] + newKey[i]) % 26) + 'A';
+ *
+ *    encryptedMsg[i] = '\0';
+ *
+ *    //decryption
+ *    for(i = 0; i < msgLen; ++i)
+ *        decryptedMsg[i] = (((encryptedMsg[i] - newKey[i]) + 26) % 26) + 'A';
+ *
+ *    decryptedMsg[i] = '\0';
+ *
+ *    printf("Original Message: %s", msg);
+ *    printf("\nKey: %s", key);
+ *    printf("\nNew Generated Key: %s", newKey);
+ *    printf("\nEncrypted Message: %s", encryptedMsg);
+ *    printf("\nDecrypted Message: %s", decryptedMsg);
+ *
+ *        return 0;
+ *}	
+ */
 	return 0;
 }
