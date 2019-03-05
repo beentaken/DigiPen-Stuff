be0b1785a48eb8cb68b8f0ca2d13a04cbce714f0
PowerUser00 <blakelysnorth@gmail.com>
2019-03-04 16:39:07 -0800

    Create StringTest.c

---
+++ 01-C/Other_Stuff_In_C/Strings/StringTest/StringTest.c
@@ -0,0 +1,21 @@
+/******************************
+ * Filename: StringTest.c
+ * Name: Blakely North
+ * Date Last Edited: 1-22-2019
+ * Brief Description: A sample and standard new file
+ * gcc -Wall -Wextra -O -ansi -pedantic -o StringTest StringTest.c
+ ******************************/
+/*printf, scanf*/
+#include <stdio.h>
+/*rand, srand*/
+#include <stdlib.h>
+/*time*/
+#include <time.h>
+
+int main(void)
+{
+  char string1[] = {'P', 'i', 'k', 'a', 'c', 'h', 'u'};
+  char *strptr = "Typhlosion";
+  char string2[] = "Koffing";
+  return 0;
+}
