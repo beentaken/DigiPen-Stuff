f6c4627a2ad4a9a1bfb5b4ecb26302e07e67240f
PowerUser00 <blakelysnorth@gmail.com>
2019-03-04 16:54:39 -0800

    Update StringTest.c

--- 01-C/Other_Stuff_In_C/Strings/StringTest/StringTest.c
+++ 01-C/Other_Stuff_In_C/Strings/StringTest/StringTest.c
@@ -3,7 +3,7 @@
  * Name: Blakely North
  * Date Last Edited: 1-22-2019
  * Brief Description: A sample and standard new file
- * gcc -Wall -Wextra -O -ansi -pedantic -o StringTest StringTest.c
+   gcc -Wall -Wextra -O -ansi -pedantic -o StringTest StringTest.c
  ******************************/
 /*printf, scanf*/
 #include <stdio.h>
@@ -14,8 +14,31 @@
 
 int main(void)
 {
+  int i, j;
   char string1[] = {'P', 'i', 'k', 'a', 'c', 'h', 'u'};
   char *strptr = "Typhlosion";
   char string2[] = "Koffing";
+
+  puts(string1);
+  puts(strptr);
+  puts(string2);
+  /*
+  bad cuz were overwritind string literal
+  strcpy(strptr, string1);
+  not enough space
+  strcpy(string1, strptr);
+  */
+  strcpy(string2, strptr);
+
+  puts(string2);
+  printf("Is %s before, after, or equal to %s lexiographically?", string1, strptr);
+  i = strcmp(string1, strptr);
+  if(i == 0)
+    puts("They are equal");
+  else if(i < 0)
+    puts("the first is first");
+  else if(i > 0)
+    puts("the latter is first");
+
   return 0;
 }
