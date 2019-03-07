9b7f0849d31287360282434887441d2a2bdda8cd
PowerUser00 <blakelysnorth@gmail.com>
2019-03-06 16:33:57 -0800

    Update NewFile.c

--- NewFile.c
+++ NewFile.c
@@ -1,6 +1,6 @@
 /******************************
  * Filename: NewFile.c
- * Name: Blakely North
+ * Author: Blakely North
  * Date Last Edited: 1-22-2019
  * Brief Description: A sample and standard new file
    gcc -Wall -Wextra -O -ansi -pedantic -o NewFile NewFile.c
