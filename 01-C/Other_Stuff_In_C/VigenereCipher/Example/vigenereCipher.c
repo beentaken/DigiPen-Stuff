921558d71a4ae835cdd50bad6272fb6999e8ec81
PowerUser00 <blakelysnorth@gmail.com>
2019-03-10 16:41:34 -0700

    Re-add in the example

---
+++ 01-C/Other_Stuff_In_C/VigenereCipher/Example/vigenereCipher.c
@@ -0,0 +1,16 @@
+/****************
+vigenereCipher.c
+By:
+Date:
+Brief:
+gcc -O -Wall -Wextra -ansi -pedantic -o vcipher vigenereCipher.c main.c
+****************/
+#include "vigenereCipher.h"
+
+void vigenere(unsigned char *string, unsigned char *key, enum CODE_METHOD method, int passes)
+{
+	
+	
+	return;
+}
+
--- 01-C/Other_Stuff_In_C/VigenereCipher/vigenereCipher.c
+++ 01-C/Other_Stuff_In_C/VigenereCipher/vigenereCipher.c
@@ -10,5 +10,59 @@
 
 void vigenere(unsigned char *string, unsigned char *key, enum CODE_METHOD method, int passes)
 {
-	return;
+	if (method == ENCODE)
+	{
+		/*ask for the string to encrypt and store it in string*/
+		printf("\nWhat phrase do you want to encrypt?\n>");
+		fgets((char *)string, 256, stdin);
+		/*ask for the key to encrypt with and store it in key*/
+		printf("\nWhat encryption key do you want to use?\n>");
+		fgets((char *)key, 32, stdin);
+
+		/*ask for how many passes to encrypt with*/
+		printf("\nHow many passes to do?\n>");
+		scanf(" %i", &passes);
+		stringlen = strlen((char *)string);
+		keylen = strlen((char *)key);
+		/*encrypt string with the key, make sure set to encode!*/
+		/* deal with capital and lowercase letters */
+
+		/*KILL ALL CAPITAL LETTERS*/
+		for (i = 0; i < stringlen; ++i)
+				string[i % stringlen] -= 32;
+
+		for (j = 0; j < passes; ++j)
+			for (i = 0; i < stringlen; string[i % stringlen] += key[i % keylen], ++i)
+				;
+
+		if (i != -1)
+			printf("The encrypted phrase is:\n%s", string);
+	}
+	else if (method == ENCODE)
+	{
+		/*ask for the string to encrypt and store it in string*/
+		printf("\nWhat phrase do you want to encrypt?\n>");
+		fgets((char *)string, 256, stdin);
+		/*ask for the key to encrypt with and store it in key*/
+		printf("\nWhat encryption key do you want to use?\n>");
+		fgets((char *)key, 32, stdin);
+
+		/*ask for how many passes to encrypt with*/
+		printf("\nHow many passes to do?\n>");
+		scanf(" %i", &passes);
+		stringlen = strlen((char *)string);
+		keylen = strlen((char *)key);
+		/*encrypt string with the key, make sure set to encode!*/
+		/* deal with capital and lowercase letters */
+
+		/*KILL ALL CAPITAL LETTERS*/
+		for (i = 0; i < stringlen; ++i)
+				string[i % stringlen] -= 32;
+
+		for (j = 0; j < passes; ++j)
+			for (i = 0; i < stringlen; string[i % stringlen] += key[i % keylen], ++i);
+
+		if (i != -1)
+			printf("The encrypted phrase is:\n%s", string);
+	}
 }
