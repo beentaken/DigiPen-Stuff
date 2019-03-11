6dbae99d0bad7a14b1ae5182ccad8317b2319e9b
PowerUser00 <blakelysnorth@gmail.com>
2019-03-11 15:58:40 -0700

    Create FileIO.c

---
+++ 01-C/Other_Stuff_In_C/FileIO/FileIO.c
@@ -0,0 +1,22 @@
+/******************************
+ * Filename: NewFile.c
+ * Author: Blakely North
+ * Date Last Edited: 1-22-2019
+ * Brief Description: A sample and standard new file
+   gcc -Wall -Wextra -O -ansi -pedantic -o NewFile NewFile.c
+ ******************************/
+/*printf, scanf*/
+#include <stdio.h>
+/*rand, srand*/
+#include <stdlib.h>
+/*time*/
+#include <time.h>
+/*strcat, strcpy etc.*/
+#include <string.h>
+
+int main(void)
+{
+  /*Counters*/
+  int i, j;
+  return 0;
+}
