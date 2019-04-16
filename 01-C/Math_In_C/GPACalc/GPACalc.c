20ee953bea80941878868fa58e00b1be2265668a
Blakely <blakelysnorth+156@gmail.com>
2019-04-15 17:41:43 -0700

    Create GPACalc.c

---
+++ 01-C/Math_In_C/GPACalc/GPACalc.c
@@ -0,0 +1,26 @@
+/******************************
+ * Filename: GPACalc.c
+ * Author: Blakely North
+ * Date Last Edited: 1-22-2019
+ * Brief Description: A sample and standard new file
+   gcc -Wall -Wextra -O -ansi -pedantic -o GPACalc GPACalc.c
+ ******************************/
+/*printf, scanf*/
+#include <stdio.h>
+/*rand, srand*/
+#include <stdlib.h>
+/*time*/
+#include <time.h>
+/*strcat, strcpy etc.*/
+#include <string.h>
+
+int main(void)
+{
+  /*Counters*/
+  int i, j;
+  //input
+  char input;
+  
+  
+  return 0;
+}
