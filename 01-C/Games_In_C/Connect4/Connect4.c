d39d3183bf2160f1789af7807a92fbcc02007302
Blakely <blakelysnorth+661@gmail.com>
2019-04-16 18:03:31 -0700

    Add most necessary functions
    Began adding function prototypes to outline the program.

--- 01-C/Games_In_C/Connect4/Connect4.c
+++ 01-C/Games_In_C/Connect4/Connect4.c
@@ -5,23 +5,41 @@
  * Brief Description: A sample and standard new file
    gcc -Wall -Wextra -O -ansi -pedantic -o GPACalc GPACalc.c
  ******************************/
-/*printf, scanf*/
+//printf, scanf
 #include <stdio.h>
-/*rand, srand*/
+//rand, srand
 #include <stdlib.h>
-/*time*/
+//time
 #include <time.h>
-/*strcat, strcpy etc.*/
+//strcat, strcpy etc.
 #include <string.h>
+//Board width
+#define WIDTH 7
+//Board height
+#define HEIGHT 6
+
+//prints the board (duh)
+int printBoard();
+//Returns an int between 1 and 7 based on what the player's move was. Also checks if it is a valid move
+int playerTurn(player);
+//Reurns 1 for player X (one), 2 for player O (two), 0 for draw, and -1 when nobody has won
+int whoWon();
+//game loop
+int gameloop();
+
+//Counters
+int i, j, x, y;
+//player input
+int input;
+//game board
+int board[WIDTH][HEIGHT];
 
 int main(void)
 {
-  /*Counters*/
-  int i, j;
-  //input
-  char input;
-
-  
-
+  int turn = 0;
+  while
+  printBoard();
+  playerTurn();
+  turn = !turn;
   return 0;
 }
